73b7292628a4677cc7cf55969849761e72690755
Icaro <161033248+Ic4ro@users.noreply.github.com>
2025-02-12 08:38:53 -0300

    Add files via upload

---
+++ Atividade while/MenuMochilas.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class MenuMochilas {
+
+    public static void main(String[]args) {
+        Scanner sc=new Scanner(System.in);
+        int op=8;
+
+        while (op!=4) {
+            System.out.println("\n===MENU===");
+            System.out.println("1- MOCHILA ESCOLAR");
+            System.out.println("2- MOCHILA DE VIAGEM");
+            System.out.println("3- MOCHILA ESPORTIVA");
+            System.out.println("4- SAIR");
+            System.out.println("ESCOLHA UMA MOCHILA: ");
+            op=sc.nextInt();
+            
+            switch (op) {
+                case 1:
+                    System.out.println("Mochila Escolar: Compacta e ideal para estudantes");
+                    break;
+                case 2:
+                    System.out.println("Mochila de Viagem: Espaçosa, ideal para longas jornadas");
+                    break;
+                case 3:
+                    System.out.println("Mochila Esportiva: Leve, resistente e ergonômica");
+                    break;
+                case 4:
+                    System.out.println("Saindo...");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        sc.close();
+    }
+}
