4afec6deda92cfeb4b209ee595f2162d41084330
Icaro <161033248+Ic4ro@users.noreply.github.com>
2025-02-12 08:59:43 -0300

    Add files via upload

---
+++ Menu/MenuCanetas.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class MenuCanetas {
+
+    public static void main(String[]args) {
+        Scanner sc=new Scanner(System.in);
+        int op=8;
+
+        while (op!=4) {
+            System.out.println("\n===MENU===");
+            System.out.println("1- CANETA ESFEROGRÁFICA");
+            System.out.println("2- CANETA GEL");
+            System.out.println("3- CANETA TINTEIRO");
+            System.out.println("4- SAIR");
+            System.out.println("ESCOLHA UMA CANETA: ");
+            op=sc.nextInt();
+            
+            switch (op) {
+                case 1:
+                    System.out.println("Caneta Esferográfica: Econômica e de longa duração");
+                    break;
+                case 2:
+                    System.out.println("Caneta Gel: Tinta mais pigmentada e escrita suave");
+                    break;
+                case 3:
+                    System.out.println("Caneta Tinteiro: Clássica e elegante, usada para caligrafia");
+                    break;
+                case 4:
+                    System.out.println("Saindo...");
+                    break;
+
+                default:
+                    System.out.println("Tente novamente com um número menor.");
+                    break;
+            }
+        }
+        sc.close();
+    }
+}
---
+++ Menu/MenuEmbalagens.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class MenuEmbalagens {
+
+    public static void main(String[]args) {
+        Scanner sc=new Scanner(System.in);
+        int op=8;
+
+        while (op!=4) {
+            System.out.println("\n===MENU===");
+            System.out.println("1- CAIXA DE PAPELÃO");
+            System.out.println("2- SACOLA PLÁSTICA");
+            System.out.println("3- EMBALAGEM DE VIDRO");
+            System.out.println("4- SAIR");
+            System.out.println("ESCOLHA UMA EMBALAGEM: ");
+            op=sc.nextInt();
+            
+            switch (op) {
+                case 1:
+                    System.out.println("Caixa de papelão: Ideal para transporte e armazenamento");
+                    break;
+                case 2:
+                    System.out.println("Sacola plástica: Leve e prática, mas pouco sustentável");
+                    break;
+                case 3:
+                    System.out.println("Embalagem de vidro: Resistente e reutilizável");
+                    break;
+                case 4:
+                    System.out.println("Saindo...");
+                    break;
+
+                default:
+                    System.out.println("Tente novamente com um número menor.");
+                    break;
+            }
+        }
+        sc.close();
+    }
+}
